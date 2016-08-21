bd9f44691a098daac4ca96185e126a44725f5a0e
José Manuel Gaytán <mti.jgaytan@gmail.com>
2016-08-21 15:11:31 -0500

    se termina el examen para mostrar varias interpretaciones para los números

--- evaluaciones.playground/Pages/semana2_memoria.xcplaygroundpage/Contents.swift
+++ evaluaciones.playground/Pages/semana2_memoria.xcplaygroundpage/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, wolrd"
+let numeros = 1...100
+
+var div5 = ""
+var par = ""
+var impar = ""
+var range = ""
+
+for i in numeros{
+    
+    div5 = i % 5 == 0 ? "Bingo!!!" : ""
+    
+    par = i % 2 == 0 ? "par!!!" : ""
+    
+    impar = i % 2 == 1 ? "impar!!!" : ""
+    
+    range = i > 30 && i < 40 ? "Viva Swift!!!" : ""
+    
+    print("# \(i) \(div5) \(par) \(impar) \(range)")
+    
+}
